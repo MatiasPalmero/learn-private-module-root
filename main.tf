fe67473ced8e321fea63960e8ee19fad8f9ef0f1
MatiasPalmero <42947186+MatiasPalmero@users.noreply.github.com>
2020-08-29 17:31:36 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 module "s3-webapp" {
   source  = "app.terraform.io/moebius/s3-webapp/aws"
   name        = var.name
